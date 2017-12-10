37fa32953c992ed0d182eba2af5e0d7a0005f73d
vinitha shree <30892388+vinitha-shree@users.noreply.github.com>
2017-12-10 13:50:27 +0530

    Add files via upload

---
+++ week-1/task.py
@@ -0,0 +1,92 @@
+
+
+from flask import Flask,render_template,request,make_response,abort
+from urllib.request import urlopen
+import json
+import logging
+
+#logging.basicConfig(filename='nam.log',level=logging.INFO  , format='%(asctime)s:%(levelname)s:%(message)s')
+
+app = Flask(__name__)
+
+class Author:
+	def __init__(self,name,id):
+		self.name = name
+		self.id = id
+		self.pCount = 0
+
+@app.route('/')                                                     #task1 - hello world
+def greet():
+    return "Hello World - Vinitha shree"
+	
+@app.route('/authors')                                              #task2 - displaying authors and their post count from json
+def fetchAndDisplay():
+	urlAuthors = "https://jsonplaceholder.typicode.com/users"
+	urlPosts = "https://jsonplaceholder.typicode.com/posts"
+	
+	print('Downloading authors')
+	with urlopen(urlAuthors) as conn:
+		aList = json.loads(conn.read().decode())
+	print('Downloading posts')
+	with urlopen(urlPosts) as conn:
+		pList = json.loads(conn.read().decode())
+			
+	#authors = [Author(aList[i]["name"],aList[i]["id"]) for i in range(len(aList))]
+	print('Setting up authors')
+	authors = []
+	for i in range(len(aList)):
+		authors.append(Author(aList[i]["name"],aList[i]["id"]))
+	
+	print("Starting Post count loop")
+	for author in authors:
+		for post in pList:
+			if post["userId"] == author.id:
+				author.pCount = author.pCount + 1
+		
+	return render_template("authors.html",authors=authors)
+
+
+@app.route("/set")                                                  #task3 - setting cookie
+def settcookie():
+    resp=make_response('setting cookie!')
+    resp.set_cookie('name','vinitha')
+    resp.set_cookie('age','20')
+    return resp
+
+
+@app.route("/get")                                                  #task4 - retreiving cookie
+def getcookie():
+    cookie1=request.cookies.get('name')
+    cookie2=request.cookies.get('age')
+    x= ("name - %s  & age - %s ") %(cookie1,cookie2)
+    
+    return x
+
+@app.route("/robot.txt")                                            #task5 - denying request to robot.txt page
+def deny():
+        abort(403)
+
+ 
+@app.route("/html")                                                 #task6 - rendering html file
+def pro():
+        return render_template("college.html")
+
+@app.route("/form")                                                 #task7 - fething user input and displaying
+def form():
+        return render_template("form.html")
+
+
+@app.route('/send' ,  methods=['POST','GET'])
+def send():
+        if  request.method ==  'POST':
+                result=[]
+                name=request.form['name']
+                result.append(name)
+                result.append(request.form['city'])
+               # logging.info("name: %s" %name)
+                return render_template('name.html' , result=result)
+        
+        return  "no name"
+
+if __name__=='__main__':
+	app.run(debug=True, port=8080)
